0116a63671172360b01c002ee230ee94218c2050
Kelly Pressley <kellypressley86@gmail.com>
2019-11-21 23:58:37 -0700

    basic layout finished

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require("express");
-var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-
-var PORT = process.env.PORT || 3000;
-
+var methodOverride = require("method-override");
 var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
@@ -21,9 +22,13 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burger_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
-console.log(`Running on http://localhost:${PORT}`);
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log(`App listening on PORT http://localhost:${PORT}`);
+  });
+});
